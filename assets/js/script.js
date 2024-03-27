0e355a62ede766036827d9b4ba27d1a385c1be70
anderson <andersondasilvabarretoasb@gmail.com>
2024-03-27 14:50:35 -0300

    teste

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ document.body.addEventListener("keyup", (event) => {
 document.querySelector(".composer button").addEventListener("click", ()=> {
     let song = document.querySelector("#input").value
     song = song.toLowerCase()
+    console.log(song)
     if(song !== "") {
         let songArray = song.split("")
         playComposition(songArray)
